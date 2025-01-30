fc2b0474156bd9e2bfe24eee3d71dd4e822f1079
Gilpe <javier.gil.perez@outlook.es>
2025-01-30 17:15:44 +0100

    Fixes

--- setup.sh
+++ setup.sh
@@ -13,24 +13,26 @@
 source lib/log.sh
 
 # Bash settings
-set -o errexit                                    # abort on nonzero exitstatus
-set -o nounset                                    # abort on unbound variable
-set -o pipefail                                   # don't hide errors within pipes
-trap 'echo "Script failed at line $LINENO: "' ERR # catch non controled exceptions
+set -o errexit  # abort on nonzero exitstatus
+set -o pipefail # don't hide errors within pipes
+set -o errtrace # ensure ERR trap is inherited
+
+# Signal catching
+trap 'log_e "Failed at line $LINENO."' ERR
 
 # Variables
 IFS=$'\n'
 debug_mode=false
-current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 declare -rA menu_options=(
     ["Package installation"]="install.sh"
     ["Configuration import"]="config.sh"
 )
+menu_choices=""
 script_name=""
 
 # Functions
 display_usage() {
-    grep "^#/" "$current_dir/${0}" | sed "s/^#\/\($\| \)//;s/SCRIPTNAME/${0##*/}/"
+    grep "^#/" "${0}" | sed "s/^#\/\($\| \)//;s/SCRIPTNAME/${0##*/}/"
 }
 
 parse_args() {
@@ -55,13 +57,14 @@ parse_args() {
 
 display_welcome() {
     echo -e "\n"
-    gum style --faint --border none --align right --width 50 \
+    gum style --faint --italic --border none --align right --width 50 \
         "Be welcome to..."
     gum style --border double --align center --width 50 --padding "1 2" --border-foreground 212 \
         "$(
-            gum style --bold --foreground 85 \
+            gum style --bold --foreground 10 \
                 "Gilpe"
         )'s package installer and dotfile setter" "for a new workstation"
+    if $debug_mode; then log_d "Debug mode is enabled."; fi
     log_i "A brief advice:"
     gum format -- "" \
         "> This script runs incrementally and on priority." \
@@ -70,26 +73,23 @@ display_welcome() {
         "> But a recomendation is to run it all at once." \
         "> Enjoy the ride! 🚀"
     echo -e "\n"
-    if $debug_mode; then log_d "Debug mode is enabled."; fi
 }
 
 display_farewell() {
     log_i "The end."
     gum style --border none --align center --width 50 --padding "1 2" \
         "See $(
-            gum style --bold --foreground 85 "you"
+            gum style --bold --foreground 10 "you"
         ) later 🫡"
 }
 
 # Run program
-if $debug_mode; then log_d "Script arguments: $*."; fi
 parse_args "${@}"
 
 display_welcome
 
 menu_choices=$(gum choose --cursor "👉 " --no-limit --header "Pick at least one process to be done:" "${!menu_options[@]}")
-if $debug_mode; then log_d "Menu choices: $menu_choices."; fi
-
+if $debug_mode; then log_d "Menu choices: [$menu_choices]."; fi
 if [ -z "$menu_choices" ]; then
     log_w "It looks like you haven't selected anything."
 else
@@ -100,7 +100,7 @@ else
             log_i "Granting execution permissions to $script_name."
             chmod +x "$script_name"
         fi
-        sudo ./"$script_name"
+        sudo ./"$script_name" "${@}"
         log_i "Finishing $choice sub-process..."
     done
 fi
