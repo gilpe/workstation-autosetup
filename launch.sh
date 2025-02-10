4a670480f608d323739a11c706f29947b879c600
Gilpe <javier.gil.perez@outlook.es>
2025-02-10 16:06:05 +0100

    better dependencies validation

--- launch.sh
+++ launch.sh
@@ -6,19 +6,19 @@ set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 set -o errtrace # ensure ERR trap is inherited
 trap 'catch $? $LINENO' ERR
+# VARIABLES _______________________________________________________________________________________
+declare -a missing_packages=()
 
 # MAIN PROGRAM ____________________________________________________________________________________
 if [ ! -f /etc/arch-release ]; then
     echo "Sorry but this only runs in Arch-based distros :("
     exit 1
 fi
-if ! pacman -Qi gum &>/dev/null; then
-    echo "==> Installing gum dependency..."
-    sudo pacman -S gum
-fi
-if ! pacman -Qi git &>/dev/null; then
-    echo "==> Installing git dependency..."
-    sudo pacman -S git
+if ! pacman -Qi gum &>/dev/null; then missing_packages+=("gum"); fi
+if ! pacman -Qi git &>/dev/null; then missing_packages+=("git"); fi
+if [ ${#missing_packages[@]} -gt 0 ]; then
+    echo "==> Installing git dependencies..."
+    sudo pacman -S "$missing_packages[@]"
 fi
 echo "==> Downloading the whole repo..."
 temp_dir=$(mktemp -d)
